7b3443e80fdbca07d48cf9db34d89d13a68eec34
caesarnerva <caesarnerva@proton.me>
2023-08-12 14:35:13 -0700

    use langchain, openAI to make simple version

--- src/app/builder/page.js
+++ src/app/builder/page.js
@@ -1,44 +1,70 @@
-'use client'
+"use client"
+// AI
+import { OpenAI } from "langchain/llms/openai";
 
-import Image from 'next/image'
-import { useState } from 'react'
+// Next, React
+import { useState, useEffect } from "react"
 
 export default function Build() {
 
-  const [persona, setPersona] = useState('#persona')
-  const [topic, setTopic] = useState('#topic')
-  const [entities, setEntities] = useState([])
-  const [isSpecificPerson, setIsSpecificPerson] = useState(false)
+  // Prompt elements
+  const [persona, setPersona] = useState("")
+  const [response, setResponse] = useState("")
 
-  let promptOpeners = [
-    `I want you to act like a ${persona}.`, `Pretend you're a ${persona}.`
-  ]
-  let promptBodies = [
-    `Explain how ${topic} works using your expertise or from your point of view.`, `Compare ${entities[0]} with ${entities[1]}.`
-  ]
-  let promptInstruction = [
-    `Please go into full detail.`, `Assume I already know the basics, and skip the introductory information.`
-  ]
+  // UX states
+  const [isEmpty, setIsEmpty] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const promptSeeker = async (e) => {
+    e.preventDefault()
+    // Turn loading on
+    setIsEmpty(false)
+    // OpenAI
+    const model = new OpenAI({
+      openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    });
+
+    const promptPrimitive =
+      `Use these examples to help me craft a prompt to learn about ${persona}. In your response, don't include the first line that lists the persona.
+      
+      Examples:
+    
+      Input: endocrinologist
+      Output: I want to learn about endocrinology. Please assume you're an authority of endocrinology and have agreed to meet me for coffee to discuss the nature and history of the discipline. Explain to me the basics of the endocrine system and your role as a health care specialist in your field.
+      
+      Input: pianist
+      Output: I want to become a pianist. Let's imagine that you are an accomplished concert pianist with decades of experience under your belt. Tell me about the profession and what it takes to excel in your field.
+      
+      Input: ${persona}
+      Output:
+      `
+
+    const result = await model.call(
+      promptPrimitive
+    );
+
+    // Set states
+    setResponse(result)
+    setIsLoading(false)
+    }
+    
+  // if (isLoading) {return <p>Loading...</p>}
   return (
     <main>
-      <h1>builder</h1>
-      <div className='text-zinc-500 text-sm'>Opener</div>
+      <form onSubmit={promptSeeker}>
+        <label>Persona:</label>
+        <input
+          type="text"
+          className="bg-zinc-800"
+          value={persona}
+          onChange={(event) => {
+            setPersona(event.target.value);
+          }}
+        />
+        <button type="submit">Create Prompt</button>
+      </form>
       {
-        promptOpeners.map((opener) =>
-          <p>{opener}</p>
-        )
-      }
-      <div className='text-zinc-500 text-sm'>Body</div>
-      {
-        promptBodies.map((body) =>
-          <p>{body}</p>
-        )
-      }
-      <div className='text-zinc-500 text-sm'>Instructions</div>
-      {
-        promptInstruction.map((instruction) =>
-          <p>{instruction}</p>
-        )
+        isEmpty ? <p>enter prompt specifics above</p> : isLoading ? <p>loading</p> : <p>{response}</p>
       }
     </main>
   )
