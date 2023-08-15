961778c4642c3ca05f4e1d56111f679a4dad4ae8
caesarnerva <caesarnerva@proton.me>
2023-08-15 15:44:50 -0700

    Make site name a link

--- components/topnav.js
+++ components/topnav.js
@@ -11,7 +11,7 @@ export default function TopNav() {
 
   return (
     <header className="flex items-center gap-8 py-2 px-6 border-b-2 border-zinc-900">
-      <h2 className="font-semibold">Prompt Explorer</h2>
+      <h2 className="font-semibold"><Link href="/">Prompt Explorer</Link></h2>
       <button className="bg-sky-500/20 hover:bg-sky-500/50 rounded-full py-2 px-4"><a href="https://buy.stripe.com/6oEg0d9PM3tHepW4gg" target="_blank"><span className="font-bold mr-2">✨ Launch Offer ✨</span>50 prompts for $10</a></button>
       <div className="flex justify-between w-32">
         <Link href="/builder" className={pathname.includes(`/builder`) ? 'bg-indigo-900' : 'bg-zinc-200'}>Builder</Link>
