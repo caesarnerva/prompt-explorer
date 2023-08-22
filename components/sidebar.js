34488ca65207efde3f00de425d2139a228b35ffa
caesarnerva <caesarnerva@proton.me>
2023-08-21 22:53:43 -0700

    Update tab names

--- components/sidebar.js
+++ components/sidebar.js
@@ -15,23 +15,23 @@ export default function Sidebar() {
           <p className="text-sm text-zinc-300">Craft detailed, topical questions.</p>
         </Link>
         <Link href='/builder/summarize' className={pathname == `/builder/summarize` ? 'p-2 border-solid border-2 border-zinc-800 rounded' : 'p-2 border-solid border-2 border-zinc-900 rounded'}>
-          <h3 className='text-md font-bold'>Summarize Text</h3>
+          <h3 className='text-md font-bold'>Summarize</h3>
           <p className="text-sm text-zinc-300">Ask the AI to simplify a piece of text.</p>
         </Link>
         <Link href='/builder/creative-writing' className={pathname == `/builder/creative-writing` ? 'p-2 border-solid border-2 border-zinc-800 rounded' : 'p-2 border-solid border-2 border-zinc-900 rounded'}>
-          <h3 className='text-md font-bold'>Creative Writing</h3>
+          <h3 className='text-md font-bold'>Write</h3>
           <p className="text-sm text-zinc-300">Use the AI as your own ghost writer.</p>
         </Link>
         <Link href='/builder/learn' className={pathname == `/builder/learn` ? 'p-2 border-solid border-2 border-zinc-800 rounded' : 'p-2 border-solid border-2 border-zinc-900 rounded'}>
-          <h3 className='text-md font-bold'>Learn a Discipline</h3>
+          <h3 className='text-md font-bold'>Learn</h3>
           <p className="text-sm text-zinc-300">Prod GPT4 to go deep on topics.</p>
         </Link>
         <Link href='/builder/debate' className={pathname == `/builder/debate` ? 'p-2 border-solid border-2 border-zinc-800 rounded' : 'p-2 border-solid border-2 border-zinc-900 rounded'}>
-          <h3 className='text-md font-bold'>Debate a Topic</h3>
+          <h3 className='text-md font-bold'>Debate</h3>
           <p className="text-sm text-zinc-300">Use the AI as your own ghost writer.</p>
         </Link>
         <Link href='/builder/historical-figures' className={pathname == `/builder/historical-figures` ? 'p-2 border-solid border-2 border-zinc-800 rounded' : 'p-2 border-solid border-2 border-zinc-900 rounded'}>
-          <h3 className='text-md font-bold'>Chat with Historical Figures</h3>
+          <h3 className='text-md font-bold'>Converse</h3>
           <p className="text-sm text-zinc-300">Coax the AI into representing real people.</p>
         </Link>
         <Link href='/builder/time-travel' className={pathname == `/builder/time-travel` ? 'p-2 border-solid border-2 border-zinc-800 rounded' : 'p-2 border-solid border-2 border-zinc-900 rounded'}>
